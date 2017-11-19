3db2be1275139675cf565557d61c235afcaa4edf
greyishmoon <chris@blythe.plus.com>
2017-11-19 19:47:18 +0000

    geoloc implemented

--- www/js/index.js
+++ www/js/index.js
@@ -23,7 +23,7 @@ function getPosition() {
 
 //called when the position is successfully determined
 function successPosition(position) {
-	
+	alert ("loc");
 	//You can find out more details about what the position obejct contains here:
 	// http://www.w3schools.com/html/html5_geolocation.asp
 	
@@ -31,10 +31,35 @@ function successPosition(position) {
 	//lets get some stuff out of the position object
 	var time = position.timestamp;
 	var latitude = position.coords.latitude;
+    
+    // Exercise 1 added...
+    var longitude = position.coords.longitude;
+    var accuracy = position.coords.accuracy;
+    var altitude = position.coords.altitude;
+    var altacc = position.coords.altitudeAccuracy;
+    var heading = position.coords.heading;
+    var speed = position.coords.speed;
+    
+    // DATE + TIME
+    // position.timestamp returuns type domTimeStamp
+	var domTimeStamp = time;
+    // convert domTimeStamp to Date which browsers recognise
+    var date = new Date(domTimeStamp);
+    // convert to formated time string
+    var time = date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
 	
 	//OK. Now we want to update the display with the correct values
 	$('#time').val("Recieved data at " + time);
-	$('#lattext').val("I should contain the latitude data...");
+    $('#longtext').val(longitude);
+	$('#lattext').val(latitude);
+    
+    
+    // Display extras
+	$('#acctext').val(accuracy);
+    $('#alttext').val(altitude);
+    $('#altacctext').val(altacc);
+    $('#headtext').val(heading);
+    $('#speedtext').val(speed);
 	
 }
 
